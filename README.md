1085f6b474078a78001b8596137f85686fc9bf1a
Julio Gil <juliogilsimon@gmail.com>
2023-11-10 15:26:35 +0100

    Desarrollo proyecto descarga API South-Park

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+## Proyecto REACT utilizando VITE
+
+- Creamos la carpeta Componentes y dentro de ella el fichero Api.jsx
+- Borramos todo el contenido de App.jsx
+  - Importamos el componente Api
+  - Llamamos al componente Api, dentro del return, para que se renderice en el navegador.
+- Borramos todo el contendio de App.css
+  - formateamos pading y margin a 0
+- En Api.jsx hacemos una llamada a la api de South-Park.
+  - Utilizando fecht descargamos su contenido en un JSON
+  - En el return recorremos el JSON con la función map y la mostramos en pantalla.
+
+---
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
---
+++ src/Componentes/Api.jsx
@@ -0,0 +1,28 @@
+﻿
+import {useState, useEffect} from "react";
+
+const Api=()=> {
+    const [data, setData]=useState([]);
+    useEffect(()=>{
+        fetch("https://spapi.dev/api/characters")
+            .then((response)=>response.json())
+            .then((responseData)=>{
+                setData(responseData.data)
+            })
+            .catch((error)=>{
+                console.error("Error al obtener los datos de la Api de South Park", error)
+            });
+    },[])
+    return(
+        <>
+            {
+                data.map(item=>(
+                    <div key={item.id}>{item.name}</div>
+                )
+                )
+            }
+        </>
+    )
+}
+
+export default Api
